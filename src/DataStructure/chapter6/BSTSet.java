3c3c973a4d8aa8a5253a733c15ca76db39c7c0ea
DNT_Greybox16 <damon_lh@163.com>
2020-10-28 20:41:16 +0800

    数据结构Chapter6

--- src/DataStructure/chapter6/BSTSet.java
+++ src/DataStructure/chapter6/BSTSet.java
@@ -1,32 +1,43 @@
 package DataStructure.chapter6;
 
 /**
+ * 用BST结构实现set集合
+ * <p>
+ * 实现Set接口，泛型参数继承Comparable类，底层私有 new BST对象并进行操作
+ *
  * @author Damon
  * @create 2020-10-28 20:10
  */
-public class BSTSet<E extends Comparable<E>> implements Set<E>{
+public class BSTSet<E extends Comparable<E>> implements Set<E> {
+
+    private BST<E> bst;
+
+    public BSTSet() {
+        bst = new BST<>();
+    }
+
     @Override
     public void add(E e) {
-
+        bst.add(e);
     }
 
     @Override
     public void remove(E e) {
-
+        bst.remove(e);
     }
 
     @Override
     public boolean contains(E e) {
-        return false;
+        return bst.contains(e);
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return bst.size();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return bst.isEmpty();
     }
 }
---
+++ src/DataStructure/chapter6/testBSTSet.java
@@ -0,0 +1,14 @@
+package DataStructure.chapter6;
+
+/**
+ * @author Damon
+ * @create 2020-10-28 20:39
+ */
+public class testBSTSet {
+
+    public static void main(String[] args) {
+        System.out.println("pride and prejudice");
+
+        
+    }
+}
