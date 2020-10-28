d961d5f7278804ac278fbf94d6a7ed58e23f5c1a
DNT_Blackbox15.6 <blackbox_nobody@163.com>
2020-10-28 20:48:02 +0800

    数据结构Chapter6

--- src/DataStructure/chapter6/testBSTSet.java
+++ src/DataStructure/chapter6/testBSTSet.java
@@ -1,5 +1,7 @@
 package DataStructure.chapter6;
 
+import java.util.ArrayList;
+
 /**
  * @author Damon
  * @create 2020-10-28 20:39
@@ -7,7 +9,32 @@ package DataStructure.chapter6;
 public class testBSTSet {
 
     public static void main(String[] args) {
-        System.out.println("pride and prejudice");
+        System.out.println("Pride and Prejudice");
+
+        ArrayList<String> words1 = new ArrayList<>();
+        if(FileOperation.readFile("pride-and-prejudice.txt", words1)) {
+            System.out.println("Total words: " + words1.size());
+
+            BSTSet<String> set1 = new BSTSet<>();
+            for (String word : words1)
+                set1.add(word);
+            System.out.println("Total different words: " + set1.getSize());
+        }
+
+        System.out.println();
+
+
+        System.out.println("A Tale of Two Cities");
+
+        ArrayList<String> words2 = new ArrayList<>();
+        if(FileOperation.readFile("a-tale-of-two-cities.txt", words2)){
+            System.out.println("Total words: " + words2.size());
+
+            BSTSet<String> set2 = new BSTSet<>();
+            for(String word: words2)
+                set2.add(word);
+            System.out.println("Total different words: " + set2.getSize());
+        }
 
         
     }
