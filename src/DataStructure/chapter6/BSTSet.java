c81f444d20ac6b46a3f5f90ca3219fb0c21492f8
DNT_Greybox16 <damon_lh@163.com>
2020-10-28 20:13:24 +0800

    数据结构Chapter6

---
+++ src/DataStructure/chapter6/BSTSet.java
@@ -0,0 +1,32 @@
+package DataStructure.chapter6;
+
+/**
+ * @author Damon
+ * @create 2020-10-28 20:10
+ */
+public class BSTSet<E extends Comparable<E>> implements Set<E>{
+    @Override
+    public void add(E e) {
+
+    }
+
+    @Override
+    public void remove(E e) {
+
+    }
+
+    @Override
+    public boolean contains(E e) {
+        return false;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
