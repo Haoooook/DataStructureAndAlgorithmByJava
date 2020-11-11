8c02781646baf848e6d183e47ae3e9dfdd0be729
DNT_Greybox16 <damon_lh@163.com>
2020-11-11 23:50:30 +0800

    数据结构Chapter7

--- src/DataStructure/chapter7/leetCode347/Solution2.java
+++ src/DataStructure/chapter7/leetCode347/Solution2.java
@@ -1,37 +1,23 @@
 package DataStructure.chapter7.leetCode347;
 
-import DataStructure.chapter7.PriorityQueue;
-
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.TreeMap;
+import java.util.*;
 
 /**
- * 347. 前 K 个高频元素
- * <p>
- * 给定一个非空的整数数组，返回其中出现频率前k高的元素。
- * <p>
- * 示例 1:
- * 输入: nums = [1,1,1,2,2,3], k = 2
- * 输出: [1,2]
- * <p>
- * 示例 2:
- * 输入: nums = [1], k = 1
- * 输出: [1]
- * <p>
- * <p>
- * 提示:
- * 你可以假设给定的k总是合理的，且 1 ≤ k ≤ 数组中不相同的元素的个数。
- * 你的算法的时间复杂度必须优于 O(n log n) ,n是数组的大小。
- * 题目数据保证答案唯一，换句话说，数组中前 k 个高频元素的集合是唯一的。
- * 你可以按任意顺序返回答案。
- * <p>
- * 使用我们实现的PriorityQueue解决问题
+ * 用java内置的java.util.PriorityQueue 底层是最小堆
  *
- * @create 2020-11-09 22:41
+ * add          增加一个元索                       如果队列已满，则抛出一个IIIegaISlabEepeplian异常
+ * remove       移除并返回队列头部的元素            如果队列为空，则抛出一个NoSuchElementException异常
+ * element      返回队列头部的元素                 如果队列为空，则抛出一个NoSuchElementException异常
+ * offer        添加一个元素并返回true             如果队列已满，则返回false
+ * poll         移除并返问队列头部的元素            如果队列为空，则返回null
+ * peek         返回队列头部的元素                 如果队列为空，则返回null
+ * put          添加一个元素                      如果队列满，则阻塞
+ * take         移除并返回队列头部的元素            如果队列为空，则阻塞
+ *
+ * @author Damon
+ * @create 2020-11-11 22:42
  */
-public class Solution {
+public class Solution2 {
 
     private class Freq implements Comparable<Freq> {
 
@@ -42,18 +28,21 @@ public class Solution {
             this.freq = freq;
         }
 
+        /*
+        java.util.PriorityQueue的底层是最小堆 判断大小的逻辑需变更
+         */
         @Override
         public int compareTo(Freq another) {
             if (this.freq < another.freq)
-                return 1;
-            else if (this.freq > another.freq)
                 return -1;
+            else if (this.freq > another.freq)
+                return 1;
             else
                 return 0;
         }
     }
 
-    public List<Integer> topKFrequent(int[] nums, int k) {
+    public int[] topKFrequent(int[] nums, int k) {
         /*
         1.使用TreeMap来保存数组的值即频次 key=值 value=频率
           如果treeMap中没有这个元素就存上，并赋value=1
@@ -77,22 +66,21 @@ public class Solution {
          */
         PriorityQueue<Freq> priorityQueue = new PriorityQueue<>();
         for (int key : map.keySet()) {
-            if (priorityQueue.getSize() < k)
-                priorityQueue.enqueue(new Freq(key, map.get(key)));
-            else if (map.get(key) > priorityQueue.getFront().freq) {
-                priorityQueue.dequeue();
-                priorityQueue.enqueue(new Freq(key, map.get(key)));
+            if (priorityQueue.size() < k)
+                priorityQueue.add(new Freq(key, map.get(key)));
+            else if (map.get(key) > priorityQueue.peek().freq) {
+                priorityQueue.poll();
+                priorityQueue.add(new Freq(key, map.get(key)));
             }
         }
         /*
-        3.最后 要返回List，使用LinkedList来接收优先队列的k个元素
-          根据习惯，进行翻转（降序排列）
+        3.最后 要返回int[]，分别弹出队列的队首元素放入int[] ret 来接收优先队列的k个元素
          */
-        LinkedList<Integer> res = new LinkedList<>();
+        int[] ret = new int[priorityQueue.size()];
         while (!priorityQueue.isEmpty())
-            res.add(priorityQueue.dequeue().e);
-        Collections.reverse(res);
-        return res;
+            for (int i = 0; i < priorityQueue.size(); i++)
+                ret[i] = priorityQueue.poll().e;
+        return ret;
     }
 
     private static void printList(List<Integer> nums) {
@@ -103,8 +91,10 @@ public class Solution {
 
     public static void main(String[] args) {
 
-        int[] nums = {1, 1, 1, 3, 2, 2, 2, 3, 2, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 5, 6, 5, 6, 5};
-        int k = 4;
+        int[] nums = {1,1,1,2,2,3};
+        int k = 2;
+//        int[] nums = {1};
+//        int k = 1;
         printList((new Solution()).topKFrequent(nums, k));
 
     }
