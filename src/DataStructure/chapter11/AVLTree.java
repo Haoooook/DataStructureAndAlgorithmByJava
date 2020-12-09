f408bd1c3ca1ed3ac68bcc78c7b797abe100dafd
Haoooook_A <damon_lh@163.com>
2020-12-09 23:19:39 +0800

    数据结构chapter11

--- src/DataStructure/chapter11/AVLTree.java
+++ src/DataStructure/chapter11/AVLTree.java
@@ -1,8 +1,273 @@
 package DataStructure.chapter11;
 
+import java.util.ArrayList;
+
 /**
+ * 解决的问题：BST有可能退化成一条单链表 效率降为O(n) 为避免这种情况发生引入''平衡因子''概念进行改进
+ * 平衡二叉树
+ * 1.任意节点的 左子树与右子树高度差（平衡因子）不超过1
+ * 2.标注节点的高度
+ * 3.也是BST 满足BST的性质
+ * 某结点的左子树与右子树的高度(深度)差即为该结点的平衡因子（BF,Balance Factor），平衡二叉树上所有结点的平衡因子只可能是 -1，0 或 1。
+ *
  * @author Haoooook
  * @create 2020-12-09 21:58
  */
-public class AVLTree {
+public class AVLTree<K extends Comparable<K>, V> {
+
+    private class Node {
+        public K key;
+        public V value;
+        public Node left, right;
+        public int height;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+            left = null;
+            right = null;
+            height = 1;
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public AVLTree() {
+        root = null;
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    //获得节点的高度
+    private int getHeight(Node node) {
+        if (node == null)
+            return 0;
+        return node.height;
+    }
+
+    //获得节点的平衡因子
+    private int getBalanceFactor(Node node) {
+        if (node == null)
+            return 0;
+        return getHeight(node.left) - getHeight(node.right);
+    }
+
+    // 向二分搜索树中添加新的元素(key, value)
+    public void add(K key, V value) {
+        root = add(root, key, value);
+    }
+
+    //判断树是否是BST
+    public boolean isBST() {
+        ArrayList<K> keys = new ArrayList<>();
+        inOrder(root, keys);
+
+        for (int i = 1; i < keys.size(); i++) {
+            if (keys.get(i - 1).compareTo(keys.get(i)) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //辅助函数 中序遍历
+    private void inOrder(Node node, ArrayList<K> keys) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left, keys);
+        keys.add(node.key);
+        inOrder(node.right, keys);
+    }
+
+    //判断是否是AVL
+    public boolean isBalanced() {
+        return isBalanced(root);
+    }
+
+    //辅助函数 判断每个节点的平衡因子都是不超过1
+    private boolean isBalanced(Node root) {
+        //空的二叉树 也是AVL 满足左右子树的高度差不超过1
+        if (root == null)
+            return true;
+
+        int balanceFactor = getBalanceFactor(root);
+        if (Math.abs(balanceFactor) > 1)
+            return false;
+
+        //如果上面判断平衡因子不超过1 那么该节点的树是满足AVL 那么继续看他的左右子节点的子树是否满足
+        return isBalanced(root.left) && isBalanced(root.right);
+
+    }
+
+
+    // 向以node为根的二分搜索树中插入元素(key, value)，递归算法
+    // 返回插入新节点后二分搜索树的根
+    private Node add(Node node, K key, V value) {
+
+        if (node == null) {
+            size++;
+            return new Node(key, value);
+        }
+
+        if (key.compareTo(node.key) < 0)
+            node.left = add(node.left, key, value);
+        else if (key.compareTo(node.key) > 0)
+            node.right = add(node.right, key, value);
+        else // key.compareTo(node.key) == 0
+            node.value = value;
+
+        //更新height
+        node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
+
+        //计算平衡因子
+        int balanceFactor = getBalanceFactor(node);
+        if (Math.abs(balanceFactor) > 1) {
+            System.out.println("Unbalanced : " + balanceFactor);
+        }
+
+        return node;
+    }
+
+    // 返回以node为根节点的二分搜索树中，key所在的节点
+    private Node getNode(Node node, K key) {
+
+        if (node == null)
+            return null;
+
+        if (key.equals(node.key))
+            return node;
+        else if (key.compareTo(node.key) < 0)
+            return getNode(node.left, key);
+        else // if(key.compareTo(node.key) > 0)
+            return getNode(node.right, key);
+    }
+
+    public boolean contains(K key) {
+        return getNode(root, key) != null;
+    }
+
+    public V get(K key) {
+
+        Node node = getNode(root, key);
+        return node == null ? null : node.value;
+    }
+
+    public void set(K key, V newValue) {
+        Node node = getNode(root, key);
+        if (node == null)
+            throw new IllegalArgumentException(key + " doesn't exist!");
+
+        node.value = newValue;
+    }
+
+    // 返回以node为根的二分搜索树的最小值所在的节点
+    private Node minimum(Node node) {
+        if (node.left == null)
+            return node;
+        return minimum(node.left);
+    }
+
+    // 删除掉以node为根的二分搜索树中的最小节点
+    // 返回删除节点后新的二分搜索树的根
+    private Node removeMin(Node node) {
+
+        if (node.left == null) {
+            Node rightNode = node.right;
+            node.right = null;
+            size--;
+            return rightNode;
+        }
+
+        node.left = removeMin(node.left);
+        return node;
+    }
+
+    // 从二分搜索树中删除键为key的节点
+    public V remove(K key) {
+
+        Node node = getNode(root, key);
+        if (node != null) {
+            root = remove(root, key);
+            return node.value;
+        }
+        return null;
+    }
+
+    private Node remove(Node node, K key) {
+
+        if (node == null)
+            return null;
+
+        if (key.compareTo(node.key) < 0) {
+            node.left = remove(node.left, key);
+            return node;
+        } else if (key.compareTo(node.key) > 0) {
+            node.right = remove(node.right, key);
+            return node;
+        } else {   // key.compareTo(node.key) == 0
+
+            // 待删除节点左子树为空的情况
+            if (node.left == null) {
+                Node rightNode = node.right;
+                node.right = null;
+                size--;
+                return rightNode;
+            }
+
+            // 待删除节点右子树为空的情况
+            if (node.right == null) {
+                Node leftNode = node.left;
+                node.left = null;
+                size--;
+                return leftNode;
+            }
+
+            // 待删除节点左右子树均不为空的情况
+
+            // 找到比待删除节点大的最小节点, 即待删除节点右子树的最小节点
+            // 用这个节点顶替待删除节点的位置
+            Node successor = minimum(node.right);
+            successor.right = removeMin(node.right);
+            successor.left = node.left;
+
+            node.left = node.right = null;
+
+            return successor;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Pride and Prejudice");
+
+        ArrayList<String> words = new ArrayList<>();
+        if (FileOperation.readFile("src/DataStructure/chapter11/pride-and-prejudice.txt", words)) {
+            System.out.println("Total words: " + words.size());
+
+            AVLTree<String, Integer> avlTree = new AVLTree<>();
+            for (String word : words) {
+                if (avlTree.contains(word))
+                    avlTree.set(word, avlTree.get(word) + 1);
+                else
+                    avlTree.add(word, 1);
+            }
+
+            System.out.println("Total different words: " + avlTree.getSize());
+            System.out.println("Frequency of PRIDE: " + avlTree.get("pride"));
+            System.out.println("Frequency of PREJUDICE: " + avlTree.get("prejudice"));
+            System.out.println("avlTree is BST ? " + avlTree.isBST());
+            System.out.println("avlTree is balanced ? " + avlTree.isBalanced());
+        }
+
+        System.out.println();
+    }
 }
--- src/DataStructure/chapter11/BST.java
+++ src/DataStructure/chapter11/BST.java
@@ -1,26 +1,30 @@
 package DataStructure.chapter11;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
 
 /**
- * 二分搜索树Binary Search Tree
- * 增加
- * 删除任意位置的节点
- * 中序前驱，中序后继---> 中序遍历 pre target succ
- * pre即target的中序前驱，succ是target的中序后继
+ * 二分搜索树
+ * 若任意节点的左子树不空，则左子树上所有节点的值均小于它的根节点的值；
+ * 若任意节点的右子树不空，则右子树上所有节点的值均大于或等于它的根节点的值；
+ * 任意节点的左、右子树也分别为二叉查找树；
  *
- * @author Damon
- * @create 2020-10-25 13:05
+ * 期望 O(log(n)) ,最坏O(n)（数列有序，树退化成线性表）。
+ * 虽然二叉查找树的最坏效率是O(n)，但它支持动态查询，
+ * 且有很多改进版的二叉查找树可以使树高为O(log(n))，从而将最坏效率降至O(log(n))，如：AVL树、红黑树等
+ *
+ * @author Haoooook
+ * @create 2020-12-09 21:58
  */
-public class BST<E extends Comparable<E>> {
+public class BST<K extends Comparable<K>, V> {
 
     private class Node {
-        public E e;
+        public K key;
+        public V value;
         public Node left, right;
 
-        public Node(E e) {
-            this.e = e;
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
             left = null;
             right = null;
         }
@@ -34,7 +38,7 @@ public class BST<E extends Comparable<E>> {
         size = 0;
     }
 
-    public int size() {
+    public int getSize() {
         return size;
     }
 
@@ -42,203 +46,71 @@ public class BST<E extends Comparable<E>> {
         return size == 0;
     }
 
-    //向根节点为node的树添加数据，递归算法
-    private Node add(Node node, E e) {
+    // 向二分搜索树中添加新的元素(key, value)
+    public void add(K key, V value) {
+        root = add(root, key, value);
+    }
 
-        //判断根节点为空，那么e存到根节点
-        //同理，当递归到叶子节点为空时 也创建新的节点接收数据并返回节点给上一层
+    // 向以node为根的二分搜索树中插入元素(key, value)，递归算法
+    // 返回插入新节点后二分搜索树的根
+    private Node add(Node node, K key, V value) {
+
         if (node == null) {
             size++;
-            return new Node(e);
+            return new Node(key, value);
         }
 
-        if (e.compareTo(node.e) < 0)
-            node.left = add(node.left, e);
-        else if (e.compareTo(node.e) > 0)
-            node.right = add(node.right, e);
+        if (key.compareTo(node.key) < 0)
+            node.left = add(node.left, key, value);
+        else if (key.compareTo(node.key) > 0)
+            node.right = add(node.right, key, value);
+        else // key.compareTo(node.key) == 0
+            node.value = value;
 
         return node;
     }
 
-    /**
-     * 添加数据e
-     *
-     * @param e
-     */
-    public void add(E e) {
-        root = add(root, e);
-    }
+    // 返回以node为根节点的二分搜索树中，key所在的节点
+    private Node getNode(Node node, K key) {
 
-    /**
-     * 二叉树是否包含元素e
-     *
-     * @param e
-     * @return
-     */
-    public boolean contains(E e) {
-        return contains(root, e);
-    }
-
-    private boolean contains(Node node, E e) {
         if (node == null)
-            return false;
+            return null;
 
-        if (e.compareTo(node.e) == 0)
-            return true;
-        else if (e.compareTo(node.e) < 0)
-            return contains(node.left, e);
-        else //(e.compareTo(node.e) > 0)
-            return contains(node.right, e);
+        if (key.equals(node.key))
+            return node;
+        else if (key.compareTo(node.key) < 0)
+            return getNode(node.left, key);
+        else // if(key.compareTo(node.key) > 0)
+            return getNode(node.right, key);
     }
 
-    /**
-     * 前序遍历
-     * 父节点---左子节点----右子节点
-     */
-    public void preOrder() {
-        preOrder(root);
+    public boolean contains(K key) {
+        return getNode(root, key) != null;
     }
 
-    private void preOrder(Node node) {
-        //终止条件
+    public V get(K key) {
+
+        Node node = getNode(root, key);
+        return node == null ? null : node.value;
+    }
+
+    public void set(K key, V newValue) {
+        Node node = getNode(root, key);
         if (node == null)
-            return;
-        //父节点的元素
-        System.out.println(node.e);
-        preOrder(node.left);
-        preOrder(node.right);
+            throw new IllegalArgumentException(key + " doesn't exist!");
+
+        node.value = newValue;
     }
 
-    /**
-     * 中序遍历(附带了一个排序效果)
-     * 左子节点---父节点---右子节点
-     */
-    public void inOrder() {
-        inOrder(root);
-    }
-
-    private void inOrder(Node node) {
-        if (node == null)
-            return;
-
-        inOrder(node.left);
-        System.out.println(node.e);
-        inOrder(node.right);
-    }
-
-    /**
-     * 后序遍历
-     * 左子节点---右子节点---父节点
-     * 典型的应用：是否内存的时候先处理节点的子节点再处理该节点
-     */
-    public void postOrder() {
-        postOrder(root);
-    }
-
-    private void postOrder(Node node) {
-        if (node == null)
-            return;
-
-        preOrder(node.left);
-        preOrder(node.right);
-        System.out.println(node.e);
-    }
-
-    /**
-     * 层序遍历（广度优先遍历）
-     * 逐层向下遍历
-     * 非递归方式实现，队列结构
-     */
-    public void levelOrder() {
-
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            Node cur = queue.remove();
-            System.out.println(cur.e);
-
-            if (cur.left != null)
-                queue.add(cur.left);
-            if (cur.right != null)
-                queue.add(cur.right);
-        }
-
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder res = new StringBuilder();
-        generateBSTString(root, 0, res);
-        return res.toString();
-    }
-
-    /**
-     * 生成以node为根节点，深度为depth的 描述二叉树的字符串
-     *
-     * @param node
-     * @param depth
-     * @param res
-     */
-    private void generateBSTString(Node node, int depth, StringBuilder res) {
-
-        if (node == null) {
-            res.append(generateDepthString(depth) + "null\n");
-            return;
-        }
-
-        res.append(generateDepthString(depth) + node.e + "\n");
-        generateBSTString(node.left, depth + 1, res);
-        generateBSTString(node.right, depth + 1, res);
-    }
-
-    private String generateDepthString(int depth) {
-        StringBuilder res = new StringBuilder();
-        for (int i = 0; i < depth; i++)
-            res.append("-->");
-        return res.toString();
-    }
-
-    /**
-     * 寻找二叉树的最小值
-     *
-     * @return
-     */
-    public E minimum() {
-        if (size == 0)
-            throw new IllegalArgumentException("BST is empty");
-        Node minNode = minimum(root);
-        return minNode.e;
-    }
-
-    /**
-     * 寻找二叉树的最小值
-     *
-     * @param node
-     * @return 最小值所在的节点
-     */
+    // 返回以node为根的二分搜索树的最小值所在的节点
     private Node minimum(Node node) {
         if (node.left == null)
             return node;
         return minimum(node.left);
     }
 
-    /**
-     * 找到最小值所在的节点，删除并返回最小值
-     *
-     * @return
-     */
-    public E removeMin() {
-        E ret = minimum();
-        root = removeMin(root);
-        return ret;
-    }
-
-    /**
-     * 删除最小值所在的节点
-     *
-     * @param node 为二叉树的根
-     * @return 删除最小值所在的节点后新的二叉树的根 node
-     */
+    // 删除掉以node为根的二分搜索树中的最小节点
+    // 返回删除节点后新的二分搜索树的根
     private Node removeMin(Node node) {
 
         if (node.left == null) {
@@ -252,105 +124,50 @@ public class BST<E extends Comparable<E>> {
         return node;
     }
 
-    /**
-     * 寻找二叉树的最大值
-     *
-     * @return
-     */
-    public E maximum() {
-        if (size == 0)
-            throw new IllegalArgumentException("BST is empty");
-        Node maxNode = maximum(root);
-        return maxNode.e;
-    }
+    // 从二分搜索树中删除键为key的节点
+    public V remove(K key) {
 
-    /**
-     * 寻找二叉树的最大值
-     *
-     * @param node
-     * @return 最小值所在的节点
-     */
-    private Node maximum(Node node) {
-        if (node.right == null)
-            return node;
-        return maximum(node.right);
-    }
-
-    /**
-     * 找到最小值所在的节点，删除并返回最大值
-     *
-     * @return
-     */
-    public E removeMax() {
-        E ret = maximum();
-        root = removeMax(root);
-        return ret;
-
-    }
-
-    /**
-     * 删除最大值所在的节点
-     *
-     * @param node 为二叉树的根
-     * @return 删除最大值所在的节点后新的二叉树的根 node
-     */
-    private Node removeMax(Node node) {
-
-        if (node.right == null) {
-            Node leftNode = node.left;
-            node.left = null;
-            size--;
-            return leftNode;
+        Node node = getNode(root, key);
+        if (node != null) {
+            root = remove(root, key);
+            return node.value;
         }
-
-        node.right = removeMax(node.right);
-        return node;
+        return null;
     }
 
-    /**
-     * 从二分搜索树中删除元素为e的节点
-     *
-     * @param e
-     */
-    public void remove(E e) {
-        root = remove(root, e);
-    }
+    private Node remove(Node node, K key) {
 
-    /**
-     * 删除以node为根的二分搜索树中 值为e的节点
-     *
-     * @param node
-     * @param e
-     * @return 删除节点后新的树的根
-     */
-    private Node remove(Node node, E e) {
         if (node == null)
             return null;
 
-        if (e.compareTo(node.e) < 0) {
-
-            node.left = remove(node.left, e);
+        if (key.compareTo(node.key) < 0) {
+            node.left = remove(node.left, key);
             return node;
-        } else if (e.compareTo(node.e) > 0) {
-
-            node.right = remove(node.right, e);
+        } else if (key.compareTo(node.key) > 0) {
+            node.right = remove(node.right, key);
             return node;
-        } else {//e ==node.e
-            //target节点左子树为空
+        } else {   // key.compareTo(node.key) == 0
+
+            // 待删除节点左子树为空的情况
             if (node.left == null) {
                 Node rightNode = node.right;
                 node.right = null;
                 size--;
                 return rightNode;
             }
-            //target节点右子树为空
+
+            // 待删除节点右子树为空的情况
             if (node.right == null) {
                 Node leftNode = node.left;
                 node.left = null;
                 size--;
                 return leftNode;
             }
-            //待删除节点左右子树均不为空
+
+            // 待删除节点左右子树均不为空的情况
+
+            // 找到比待删除节点大的最小节点, 即待删除节点右子树的最小节点
+            // 用这个节点顶替待删除节点的位置
             Node successor = minimum(node.right);
             successor.right = removeMin(node.right);
             successor.left = node.left;
@@ -361,4 +178,27 @@ public class BST<E extends Comparable<E>> {
         }
     }
 
+    public static void main(String[] args) {
+
+        System.out.println("Pride and Prejudice");
+
+        ArrayList<String> words = new ArrayList<>();
+        if (FileOperation.readFile("src/DataStructure/chapter11/pride-and-prejudice.txt", words)) {
+            System.out.println("Total words: " + words.size());
+
+            BST<String, Integer> map = new BST<>();
+            for (String word : words) {
+                if (map.contains(word))
+                    map.set(word, map.get(word) + 1);
+                else
+                    map.add(word, 1);
+            }
+
+            System.out.println("Total different words: " + map.getSize());
+            System.out.println("Frequency of PRIDE: " + map.get("pride"));
+            System.out.println("Frequency of PREJUDICE: " + map.get("prejudice"));
+        }
+
+        System.out.println();
+    }
 }
